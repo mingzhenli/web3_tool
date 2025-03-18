fcd628c32ace53c5a0b152ea9c59c6497ce054ef
Mzl <1132659586@qq.com>
2025-03-18 21:01:44 +0800

    `代码调整`

--- service/binance_service.py
+++ service/binance_service.py
@@ -27,10 +27,10 @@ class BinanceService:
 
     def with_draw(self,address):
         # 提币参数
-        asset = 'SUI'  # 币种，如 BTC、ETH
-        amount = 4.3  # 提币数量
+        asset = 'ETH'  # 币种，如 BTC、ETH
+        amount = 0.0144  # 提币数量
         # address = "your_wallet_address"  # 提币地址
-        network = "SUI"  # 提币网络，如 BTC、BSC
+        network = "ARBITRUM"  # 提币网络，如 BTC、BSC
         address_tag = None  # 标签（如 XRP、XMR 等需要标签）
         try:
             result = self.binance_client.withdraw(
